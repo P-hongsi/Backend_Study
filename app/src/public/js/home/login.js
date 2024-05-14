0bfedaf21dd2e9e0e845b0ba192480e8e10a1910
박홍시 <bakjy0312@gmail.com>
2024-05-14 09:00:56 +0900

    Add:로그인 에러 메세지

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,32 +1,36 @@
 'use strict';
 
 const id = document.querySelector('#id'),
-    pw = document.querySelector('#pw'),
-    btn = document.querySelector('#btn');
+  pw = document.querySelector('#pw'),
+  btn = document.querySelector('#btn');
 
 btn.addEventListener('click', () => {
-    if (!id.value || !pw.value) {
-        alert('아이디와 비밀번호를 입력해주세요!');
-        return;
-    } else {
-        const req = {
-            id: id.value,
-            pw: pw.value
+  if (!id.value || !pw.value) {
+    alert('아이디와 비밀번호를 입력해주세요!');
+    return;
+  } else {
+    const req = {
+      id: id.value,
+      pw: pw.value,
+    };
+    fetch('/login', {
+      body: JSON.stringify(req),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.success) {
+          location.href = '/';
+          alert(res.message);
+        } else {
+          alert(res.message);
         }
-        fetch("/login", {
-            body: JSON.stringify(req),
-            headers: {
-                "Content-Type": "application/json"
-            },
-            method: "POST"
-        }).then(res => res.json())
-            .then(res => {
-                if (res.success) {
-                    location.href = '/';
-                    alert(res.message);
-                } else {
-                    alert(res.message);
-                }
-            })
-    }
-})
+      })
+      .catch((err) => {
+        console.error('로그인 중 에러 발생');
+      });
+  }
+});
